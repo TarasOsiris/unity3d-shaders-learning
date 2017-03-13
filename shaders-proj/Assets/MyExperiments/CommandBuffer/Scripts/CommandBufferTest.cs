c6758b074bdee562f4ee2ea820ec1ee0c5637458
Taras Leskiv <leskiv.taras@gmail.com>
2017-03-13 23:08:56 +0100

    command buffer

---
+++ shaders-proj/Assets/MyExperiments/CommandBuffer/Scripts/CommandBufferTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+public class CommandBufferTest : MonoBehaviour
+{
+	public MeshFilter meshFilter;
+
+	CommandBuffer _commandBuffer;
+
+	Material _mat;
+
+	void OnEnable()
+	{
+		_mat = new Material(Shader.Find("MakinStuffLookGood/GlowCmdShader"));
+		_mat.SetColor("_GlowColor", Color.red);
+
+		_commandBuffer = new CommandBuffer();
+		_commandBuffer.name = "My Custom CommandBuffer";
+
+		GetComponent<Camera>().AddCommandBuffer(CameraEvent.BeforeImageEffects, _commandBuffer);
+	}
+
+	void OnDisable()
+	{
+		GetComponent<Camera>().RemoveAllCommandBuffers();
+	}
+
+	void Update()
+	{
+		_commandBuffer.ClearRenderTarget(true, true, Color.green);
+		_commandBuffer.DrawMesh(meshFilter.sharedMesh, meshFilter.transform.localToWorldMatrix, _mat);
+	}
+}
